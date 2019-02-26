e760d2f262e303efa5a74e3a58e55f18573b7414
sefadogan <sefadogan7@gmail.com>
2019-02-26 22:20:05 +0300

    Unit of Work Pattern added.

---
+++ TelephoneDirectory/TelephoneDirectory.BLL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelephoneDirectory.BLL.Abstract;
+
+namespace TelephoneDirectory.BLL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IEmployeeRepository EmployeeRepository { get; }
+       
+        bool SaveChanges();
+    }
+}
---
+++ TelephoneDirectory/TelephoneDirectory.BLL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelephoneDirectory.BLL.Abstract;
+using TelephoneDirectory.BLL.Concrete;
+using TelephoneDirectory.DAL.Entities;
+
+namespace TelephoneDirectory.BLL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TelephoneDirectoryEntities _context;
+        private IEmployeeRepository employeeRepository;
+
+        public UnitOfWork(TelephoneDirectoryEntities context)
+        {
+            _context = context;
+        }
+
+        public IEmployeeRepository EmployeeRepository => employeeRepository ?? (employeeRepository = new EmployeeRepository(_context));
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+                GC.SuppressFinalize(this);
+            }
+        }
+        public bool SaveChanges()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true; 
+            }
+            catch (Exception)
+            {
+                // Logging must add here.
+                return false;
+            }
+        }
+    }
+}
