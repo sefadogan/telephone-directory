5398ebb3bd8abcc430f4701fc779d02a047fb69e
sefadogan <sefadogan7@gmail.com>
2019-02-26 23:58:30 +0300

    Employee list and detail pages added. And some edits.

---
+++ TelephoneDirectory/TelephoneDirectory.Model/ViewModel/VMEmployee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelephoneDirectory.DAL.Entities;
+
+namespace TelephoneDirectory.Model.ViewModel
+{
+    public class VMEmployee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Telephone { get; set; }
+
+        public static VMEmployee Parse(Employee item)
+        {
+            VMEmployee result = new VMEmployee
+            {
+                EmployeeId = item.EmployeeId,
+                FirstName = item.FirstName,
+                LastName = item.LastName,
+                Telephone = item.Telephone
+            };
+
+            return result;
+        }
+        public static List<VMEmployee> Parse(List<Employee> items)
+        {
+            List<VMEmployee> results = new List<VMEmployee>();
+            foreach (var item in items)
+            {
+                if (item.Role.Name.Contains("Admin")) // UI'de sadece normal çalışanları göstermek adına.
+                    continue;
+                    
+                results.Add(VMEmployee.Parse(item));
+            }
+
+            return results;
+        }
+    }
+}
---
+++ TelephoneDirectory/TelephoneDirectory.UI/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelephoneDirectory.BLL.UnitOfWork;
+using TelephoneDirectory.DAL.Entities;
+using TelephoneDirectory.Model.ViewModel;
+
+namespace TelephoneDirectory.UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        readonly TelephoneDirectoryEntities _context;
+        readonly UnitOfWork _uow;
+
+        public EmployeeController()
+        {
+            _context = new TelephoneDirectoryEntities();
+            _uow = new UnitOfWork(_context);
+        }
+
+        public ActionResult List()
+        {
+            List<Employee> employees = _uow.EmployeeRepository.ListAll(x => x.IsActive);
+
+            if (employees.Count == 0)
+            {
+                TempData["ProcessResult"] = "There are no employees to be listed.";
+                TempData["AlertType"] = "info";
+                return View();
+            }
+
+            List<VMEmployee> vmEmployee = VMEmployee.Parse(employees);
+
+            return View(vmEmployee);
+        }
+        public ActionResult Detail(int id)
+        {
+            Employee employee = _uow.EmployeeRepository.BringById(id);
+            if(employee == null)
+            {
+                TempData["ProcessResult"] = "There are no employees to be displayed.";
+                TempData["AlertType"] = "info";
+                return View();
+            }
+
+            return View(employee);
+        }
+    }
+}
